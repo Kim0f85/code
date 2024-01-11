505a441c2aa13f02aa2bab2609d6cd83ea39ade5
Kim0f85 <shk19990314@gmail.com>
2024-01-11 18:14:25 +0900

    update0111afterhour

--- java/JavaTest/src/com/test/question/Q066.java
+++ java/JavaTest/src/com/test/question/Q066.java
@@ -1,45 +1,27 @@
 package com.test.question;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q066 {
 	public static void main(String[] args) {
-		//Scanner scan = new Scanner(System.in);
-		//System.out.print("최대 범위: ");
-		int max = 45;
-		//System.out.print("최소 범위: ");
-		int min = 1;
-
+		
 		int[] input = new int[6];
-		String auto = "";
-		String autorange = "";
 		for (int i = 0; i < input.length; i++) {
-			int random = (int) ((Math.random() * (45 - 1)) + 1);
+			int random = (int) ((Math.random() * (45)) + 1);
 			input[i] = random;
-			if (i == input.length - 1) {
-				if(input[i]==input[i-1])
-				
-				autorange += checker(input[i], max, min);
-			} else {
-				//auto += input[i] + ", ";
-				autorange += checker(input[i], max, min);
+
+			for (int j = 0; j < i; j++) {
+				if (input[i] == input[j]) {
+					i--;
+					break;
+				}
 			}
 
 		}
-		System.out.printf("원본: %s\n", auto);
-		// Arrays.sort(input);
-		System.out.printf("결과: %s\n", autorange);
+		Arrays.sort(input);
+		System.out.println(Arrays.toString(input));
+//		
 	}
 
-	private static String checker(int i, int max, int min) {
-		String result = "";
-		if (i <= max && i >= min) {
-
-			result = i + ", ";
-		} else {
-			return result;
-		}
-
-		return result;
-	}
 }
