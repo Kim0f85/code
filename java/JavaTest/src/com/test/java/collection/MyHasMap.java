01888e23f3d64ca3b54070fa3c6ec82d9304b691
Kim0f85 <shk19990314@gmail.com>
2024-01-17 17:05:44 +0900

    update 0117 end

---
+++ java/JavaTest/src/com/test/java/collection/MyHasMap.java
@@ -0,0 +1,16 @@
+package com.test.java.collection;
+
+public class MyHasMap {
+	
+	//key + value 
+	//Case 1.
+	
+	private String[] key;
+	private String[] value;
+	private int index;
+}
+
+class Entry {
+	private String key;
+	private String valueString 
+}
