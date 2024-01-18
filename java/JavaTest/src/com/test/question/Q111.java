3e1cb5fbdc1b625bf5bc01a2068d2840bbb16d0c
Kim0f85 <shk19990314@gmail.com>
2024-01-18 17:42:16 +0900

    update0118endfin

--- java/JavaTest/src/com/test/question/Q111.java
+++ java/JavaTest/src/com/test/question/Q111.java
@@ -1,5 +1,16 @@
 package com.test.question;
 
-public class Q111 {
+import java.util.Scanner;
 
+public class Q111 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);	
+		 System.out.print("파일 경로: ");
+		 String path = scan.nextLine();
+		 String fileName = "";
+		
+		 String fileType= "";
+		 
+		 
+	}
 }
